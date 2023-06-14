020445e0327da65ef06d2a98281049ea3eebf748
Vlad <balanbiz@yandex.ru>
2023-06-14 19:03:41 +0300

    Mob ready

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -10,13 +10,8 @@ import { fetchRedis } from "@/helpers/redis";
 import { getFriendsByUserId } from "@/helpers/get-friends-by-user-id";
 import { Sidebar } from "lucide-react";
 import SidebarChatList from "@/components/SidebarChatList";
-
-interface SidebarOption {
-    id: number;
-    name: string;
-    href: string;
-    Icon: Icon;
-}
+import MobileChatLayout from "@/components/MobileChatLayout";
+import { SidebarOption } from "@/types/typings";
 
 const sidebarOptions: SidebarOption[] = [
     {
@@ -36,6 +31,14 @@ export default async function DashLayout({ children }: { children: React.ReactNo
     const unseenRequestCount = ((await fetchRedis("smembers", `user:${session.user.id}:incoming_friend_requests`)) as User[]).length;
     return (
         <div className="w-full flex h-screen">
+            <div className="md:hidden">
+                <MobileChatLayout
+                    friends={friends}
+                    session={session}
+                    sidebarOptions={sidebarOptions}
+                    unseenRequestCount={unseenRequestCount}
+                />
+            </div>
             <div className="hidden md:flex h-full w-full max-w-xs grow flex-col gap-y-5 overflow-y-auto border-r border-gray-200 bg-white px-6">
                 <Link href="/dashboard" className="flex h-16 shrink-0 items-center">
                     <Icons.Logo className="h-8 w-auto text-indigo-600" />
